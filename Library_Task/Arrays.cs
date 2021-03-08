9aaa476772a7cf3ee939e3286f5305527fb475a2
creatorQuen <spastime@yandex.ru>
2021-03-08 23:34:30 +0300

    Написать метод для 1 задачи массивы ("GetMinimumNumberInArray").

--- Library_Task/Arrays.cs
+++ Library_Task/Arrays.cs
@@ -6,6 +6,26 @@ namespace Library_Task
 {
     public static class Arrays
     {
+        public static int GetMinimumNumberInArray(int[] array)
+        {
+            /// <summary>
+            /// Найти минимальный элемент массива.
+            /// </summary>
+
+            int minimum = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < minimum)
+                {
+                    minimum = array[i];
+                }
+            }
+
+            return minimum;
+        }
+
+
 
     }
 }
