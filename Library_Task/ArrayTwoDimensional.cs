cdd5bcef98bead6f0fe04f4391ea6e08a1d92ffe
creatorQuen <spastime@yandex.ru>
2021-03-13 14:53:10 +0300

    Оформить методы FindMinimumElementOfArray2D, FindMaximumElementOfArray2D, FindMInimumIndexNumberOfArray2D, FindMaximumIndexNumberOfArray2D.

--- Library_Task/ArrayTwoDimensional.cs
+++ Library_Task/ArrayTwoDimensional.cs
@@ -6,8 +6,129 @@ namespace Library_Task
 {
     public static class ArrayTwoDimensional
     {
+        public static int FindMinimumElementOfArray2D(int[,] arr)
+        {
+            /// <summary>
+            /// Найти минимальный элемент массива.
+            /// </summary>
+
+            if (arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
+            int minimum = arr[0, 0];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < minimum)
+                    {
+                        minimum = arr[i, j];
+                    }
+                }
+            }
+
+            return minimum;
+        }
+
+        public static int FindMaximumElementOfArray2D(int[,] arr)
+        {
+            /// <summary>
+            /// Найти максимальный элемент массива.
+            /// </summary>
+
+            if (arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
+            int maximum = arr[0, 0];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > maximum)
+                    {
+                        maximum = arr[i, j];
+                    }
+                }
+            }
+
+            return maximum;
+        }
+
+        public static int[] FindMInimumIndexNumberOfArray2D(int[,] arr)
+        {
+            /// <summary>
+            /// Найти индекс минимального элемента массива.
+            /// </summary>
+
+            if (arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
+            int minIndexI = 0;
+            int minIndexJ = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] < arr[minIndexI, minIndexJ])
+                    {
+                        minIndexI = i;
+                        minIndexJ = j;
+                    }
+                }
+            }
+
+            return new int[] { minIndexI + 1, minIndexJ + 1 };
+        }
+
+        public static int[] FindMaximumIndexNumberOfArray2D(int[,] arr)
+        {
+            /// <summary>
+            /// Найти индекс максимального элемента массива.
+            /// </summary>
+
+            if (arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
+            int maxIndexI = 0;
+            int maxIndexJ = 0;
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    if (arr[i, j] > arr[maxIndexI, maxIndexJ])
+                    {
+                        maxIndexI = i;
+                        maxIndexJ = j;
+                    }
+                }
+            }
+
+            return new int[] { maxIndexI + 1, maxIndexJ + 1 };
+        }
+
         public static int CountNumberOfElementsGreaterAllNeighbors(int[,] arr)
         {
+            /// <summary>
+            /// Найти количество элементов массива, которые больше всех своих соседей одновременно.
+            /// </summary>
+
+            if (arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
             int counter = 0;
 
             for(int i = 0; i < arr.GetLength(0); i++)
@@ -34,6 +155,11 @@ namespace Library_Task
             /// Отразите массив относительно его главной диагонали.
             /// </summary>
 
+            if(arr.Length == 0)
+            {
+                throw new Exception("Пустой массив.");
+            }
+
             int[,] tempArr = new int[arr.GetLength(1), arr.GetLength(0)];
 
             for (int i = 0; i < tempArr.GetLength(0); i++)
@@ -41,9 +167,7 @@ namespace Library_Task
                 for (int j = 0; j < tempArr.GetLength(1); j++)
                 {
                     tempArr[i, j] = arr[j, i];
-
                 }
-
             }
 
             return tempArr;
--- Library_Task/Program.cs
+++ Library_Task/Program.cs
@@ -9,29 +9,30 @@ namespace Library_Task
             //var arr = ProgrammingCycles.GetNumberOfPositiveIntegersWhoseSquareIsLess(-40);
 
             //int[] temp = new int[] { 2123, 2, 17, 445, 5, -65 };
-            int[,] temp = new int[,]   {{1, 2, 4, 5 },
+            int[,] temp = new int[,]   {
+                                        {1, 2, 4, 5 },
                                         { 7, 0, 9 ,4},
                                         { 5, 67, 7, -1},
                                         { 567, 34, 123, 68}
                                         };
-            var arr = ArrayTwoDimensional.GetReverseArray2D(temp);
+            var arr = ArrayTwoDimensional.FindMInimumIndexNumberOfArray2D(temp);
 
-            // Console.WriteLine(arr);
+            //Console.WriteLine(arr);
 
-            //for (int i = 0; i < arr.Length; i++)
-            //{
-            //    Console.Write($"{arr[i]} ");
-            //}
-
-
-            for (int i = 0; i < arr.GetLength(0); i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                {
-                    Console.Write($"{arr[i, j]} ");
-                }
-                Console.WriteLine("");   
+                Console.Write($"{arr[i]} ");
             }
+
+
+            //for (int i = 0; i < arr.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < arr.GetLength(1); j++)
+            //    {
+            //        Console.Write($"{arr[i, j]} ");
+            //    }
+            //    Console.WriteLine("");   
+            //}
 
 
 
