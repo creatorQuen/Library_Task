7767ec03fab8941c2e20f34dcc1ce3799189a685
creatorQuen <spastime@yandex.ru>
2021-03-13 17:13:15 +0300

    Написать тесты для методов класса Arrays: SumArrayElementsWithOddIndex, GetArrayReverse, SumNumberOddArrayElements, GetArrayChangeHalfNumber, SortArrayBubbleUp, SortArraySelectDown.

--- Library_Task.Tests/ArraysTests.cs
+++ Library_Task.Tests/ArraysTests.cs
@@ -4,10 +4,10 @@ namespace Library_Task.Tests
 {
     public class ArraysTests
     {
-        [TestCase(new int[] { -7, -28 , 100, 1, 0 , 25, 2}, -28)]
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, -28)]
         [TestCase(new int[] { 0 }, 0)]
-        [TestCase(new int[] { 1, 0, 1, 0, -1, 2}, -1)]
-        [TestCase(new int[] { -11, -11, -11, -11, -11, -11}, -11)]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, -1)]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, -11)]
         public void FindMinimumNumberInArrayTests(int[] arr, int expected)
         {
             int actual = Arrays.FindMinimumNumberInArray(arr);
@@ -52,6 +52,75 @@ namespace Library_Task.Tests
         }
 
 
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, -2)]
+        [TestCase(new int[] { 0 }, 0)]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, 2)]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, -33)]
+        public void SumArrayElementsWithOddIndex(int[] arr, int expected)
+        {
+            int actual = Arrays.SumArrayElementsWithOddIndex(arr);
 
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, new int[] { 2, 25, 0, 1, 100, -28, -7 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, new int[] { 2, -1, 0, 1, 0, 1 })]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, new int[] { -11, -11, -11, -11, -11, -11 })]
+        public void GetArrayReverseTests(int[] arr, int[] expected)
+        {
+            int[] actual = Arrays.GetArrayReverse(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, 3)]
+        [TestCase(new int[] { 0 }, 0)]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, 3)]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, 6)]
+        public void SumNumberOddArrayElementsTests(int[] arr, int expected)
+        {
+            int actual = Arrays.SumNumberOddArrayElements(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, new int[] { 0, 25, 2, 1, -7, -28, 100 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, new int[] { 0, -1, 2, 1, 0, 1 })]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, new int[] { -11, -11, -11, -11, -11, -11 })]
+        public void GetArrayChangeHalfNumberTests(int[] arr, int[] expected)
+        {
+            int[] actual = Arrays.GetArrayChangeHalfNumber(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, new int[] { -28, -7, 0, 1, 2, 25, 100 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, new int[] { -1, 0, 0, 1, 1, 2 })]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, new int[] { -11, -11, -11, -11, -11, -11 })]
+        public void SortArrayBubbleUpTests(int[] arr, int[] expected)
+        {
+            int[] actual = Arrays.SortArrayBubbleUp(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestCase(new int[] { -7, -28, 100, 1, 0, 25, 2 }, new int[] { 100, 25, 2, 1, 0, -7, -28 })]
+        [TestCase(new int[] { 0 }, new int[] { 0 })]
+        [TestCase(new int[] { 1, 0, 1, 0, -1, 2 }, new int[] { 2, 1, 1, 0, 0, -1 })]
+        [TestCase(new int[] { -11, -11, -11, -11, -11, -11 }, new int[] { -11, -11, -11, -11, -11, -11 })]
+        public void SortArraySelectDownTests(int[] arr, int[] expected)
+        {
+            int[] actual = Arrays.SortArraySelectDown(arr);
+
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
